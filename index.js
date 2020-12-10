02f15a7823c7b4eb2c76c70fec5f2447ef0450ea
bguilas <bryanjguilas@gmail.com>
2020-12-09 22:34:10 -0800

    added lodash library and caporal

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import chokidar from 'chokidar';
+import debounce from 'lodash.debounce';
+import program from 'caporal';
+
+const start = debounce(() => {
+    console.log('Starting user program');
+}, 100);
 
 chokidar.watch('.')
-    .on('add', () => console.log('File Added'))
+    .on('add', start)
     .on('change', () => console.log('File Changed'))
     .on('unlink', () => console.log('File Unlinked'));
