c8239b9e8ec977cbd2d18a93439e48194ddbf25e
bguilas <bryanjguilas@gmail.com>
2020-12-11 22:51:05 -0800

    added caporal

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ import chokidar from 'chokidar';
 import debounce from 'lodash.debounce';
 import program from 'caporal';
 
-const start = debounce(() => {
-    console.log('Starting user program');
-}, 100);
 
-chokidar.watch('.')
-    .on('add', start)
-    .on('change', () => console.log('File Changed'))
-    .on('unlink', () => console.log('File Unlinked'));
+program
+    .version('0.0.1')
+    .argument('[filename]', 'Name of a file to execute')
+    .action((args) => {
+        const start = debounce(() => {
+            console.log('Starting user program');
+        }, 100);
+
+        chokidar.watch('.')
+            .on('add', start)
+            .on('change', start)
+            .on('unlink', start);
+    });
+
+program.parse(process.argv);
+
